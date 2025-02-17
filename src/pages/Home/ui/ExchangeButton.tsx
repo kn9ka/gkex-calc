46da261b166d6ca0eb9eea00c2a7b1f4b3fdc6bb
Anton Krackov <antonkrachkov@hollandandbarrett.com>
2025-02-17 22:58:27 +0400

    feat: add backbutton logic

--- src/pages/Home/ui/ExchangeButton.tsx
+++ src/pages/Home/ui/ExchangeButton.tsx
@@ -1,16 +1,42 @@
 import styles from './ExchangeButton.module.css';
 import type { ExchangeButtonProps } from './ExchangeButton.types';
+import { motion, useAnimate, useAnimationControls } from 'framer-motion';
 
 export const ExchangeButton: React.FC<ExchangeButtonProps> = ({
   onClick,
   disabled,
 }) => {
+  const [scope, animate] = useAnimate();
+
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    await animate(
+      scope.current,
+      { scale: 1 },
+      { type: 'spring', stiffness: 1000 },
+    );
+    await animate(
+      scope.current,
+      { scale: 0.8 },
+      { type: 'spring', stiffness: 500 },
+    );
+    await animate(
+      scope.current,
+      { scale: 1 },
+      { type: 'spring', stiffness: 500 },
+    );
+    onClick(e);
+  };
+
   return (
-    <div className={styles.container}>
+    <motion.div className={styles.container} ref={scope}>
       <i className={styles.gradient} />
-      <button className={styles.button} onClick={onClick} disabled={disabled}>
+      <button
+        className={styles.button}
+        onClick={handleClick}
+        disabled={disabled}
+      >
         Exchange <em>NOW</em>
       </button>
-    </div>
+    </motion.div>
   );
 };
--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -5,7 +5,7 @@ import { Switch } from './Switch';
 import { ExchangeButton } from './ExchangeButton';
 import { ExchangeRate } from './ExchangeRate';
 import { Contact } from './Contact';
-
+import { motion } from 'framer-motion';
 import { useSubmit } from '@remix-run/react';
 
 const Currency = {
@@ -32,9 +32,10 @@ export const Home = () => {
   const submit = useSubmit();
   const [currencyIn, setCurrencyIn] = useState<Currency>(Currency.USDT);
   const [currencyOut, setCurrencyOut] = useState<Currency>(Currency.CASH);
-
   const [amountIn, setAmountIn] = useState<number>(1);
   const [contact, setContact] = useState<string>('');
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
   const swap = () => {
     setCurrencyIn(currencyOut);
     setCurrencyOut(currencyIn);
@@ -53,41 +54,55 @@ export const Home = () => {
       },
       { method: 'post' },
     );
+    setIsSubmitted(true);
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <ExchangeInput
-          title="You pay"
-          imgSrc={CurrencyList[currencyIn].icon}
-          value={amountIn}
-          onChange={setAmountIn}
-          currencyLabel={CurrencyList[currencyIn].title}
-        />
-
-        <Switch onClick={swap} />
-
-        <ExchangeInput
-          title="You receive"
-          imgSrc={CurrencyList[currencyOut].icon}
-          value={amount}
-          disabled
-          currencyLabel={CurrencyList[currencyOut].title}
-        />
-
-        <ExchangeRate
-          title={CurrencyList[currencyIn].title}
-          rate={CurrencyList[currencyOut].rate.toString()}
-        />
-
-        {amountIn > 1 && <Contact onChange={setContact} value={contact} />}
-
-        <ExchangeButton
-          onClick={handleSubmit}
-          disabled={amountIn <= 1 || !contact}
-        />
-      </div>
-    </div>
+    <motion.div
+      className={styles.container}
+      transition={{ type: 'spring', duration: 0.6 }}
+    >
+      <motion.div className={styles.content}>
+        {!isSubmitted ? (
+          <>
+            <ExchangeInput
+              title="You pay"
+              imgSrc={CurrencyList[currencyIn].icon}
+              value={amountIn}
+              onChange={setAmountIn}
+              currencyLabel={CurrencyList[currencyIn].title}
+            />
+            <Switch onClick={swap} />
+            <ExchangeInput
+              title="You receive"
+              imgSrc={CurrencyList[currencyOut].icon}
+              value={amount}
+              disabled
+              currencyLabel={CurrencyList[currencyOut].title}
+            />
+            <ExchangeRate
+              title={CurrencyList[currencyIn].title}
+              rate={CurrencyList[currencyOut].rate.toString()}
+            />
+            {amountIn > 1 && <Contact onChange={setContact} value={contact} />}
+            <ExchangeButton
+              onClick={handleSubmit}
+              disabled={amountIn <= 1 || !contact}
+            />
+          </>
+        ) : (
+          <div className={styles.submitted}>
+            <span>THANK YOU!</span>
+            <span>We will contact you soon</span>
+            <button
+              onClick={() => setIsSubmitted(false)}
+              className={styles.backButton}
+            >
+              Back to exchange
+            </button>
+          </div>
+        )}
+      </motion.div>
+    </motion.div>
   );
 };

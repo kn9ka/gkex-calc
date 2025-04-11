a04dfad9e135887f2056d64f35899c30cdcb3380
Anton Krackov <antonkrachkov@hollandandbarrett.com>
2025-04-11 21:16:53 +0400

    feat: update contacts

--- src/pages/Contacts/ui/mock.tsx
+++ src/pages/Contacts/ui/mock.tsx
@@ -3,18 +3,18 @@ export const CONTACTS = [
     id: 1,
     logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1280px-Telegram_2019_Logo.svg.png',
     title: 'Telegram',
-    link: 'https://t.me/gkex'
+    link: 'https://t.me/gk_exchange',
   },
   {
     id: 2,
     logo: '/email.svg',
     title: 'Email',
-    link: 'gkex@gmail.com'
+    link: 'Info@gk.exchange',
   },
   {
     id: 3,
     logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1280px-Instagram_logo_2022.svg.png',
     title: 'Instagram',
-    link: 'https://www.instagram.com/gkex/'
+    link: 'https://www.instagram.com/gkex_exchange',
   },
 ];
